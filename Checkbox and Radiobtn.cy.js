c8bda3761dc4760ebf54666e081a0c2a96ec384d
Preethi S <preethi@qaoncloud.com>
2024-11-23 17:27:36 +0530

    Update Checkbox and Radiobtn.cy.js
    Added missing it (Dropdown) block

--- Checkbox and Radiobtn.cy.js
+++ Checkbox and Radiobtn.cy.js
@@ -23,9 +23,39 @@ describe('Checkboxes', () => {
 
     cy.get("[type='radio']").last().check()  // Select the last radio button
     .should('be.checked');  // Verify that it is checked
-
-
     })
+it('sample 2', ()=>{
+cy.visit('https://demo.guru99.com/test/radio.html')
+
+// Radio Button
+cy.get("#vfb-7-1").check() // id locator so we don't want to use first or last method
+cy.get("[type='radio']").last().check()
+
+// Check box
+cy.get("[type='checkbox']").first().check()
+cy.get("[type='checkbox']").last().check()
+
+cy.get("[type='checkbox']").first().uncheck()
+cy.get("[type='checkbox']").last().uncheck()
+})
+
+// Dropdown
+it.only('Dropdown', ()=>{
+
+  cy.visit('https://webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html')
+  
+  cy.get("#dropdowm-menu-1").select('sql')
+  cy.get("#dropdowm-menu-1").should('have.value','sql')
+
+  cy.get('#dropdowm-menu-1').select('Python')
+  cy.get('#dropdowm-menu-1').should('have.value','python')
+  
+  cy.get('#dropdowm-menu-2').select('Maven')
+  cy.get('#dropdowm-menu-2').should('have.value','maven')
+  
+  cy.get('#dropdowm-menu-3').select('JavaScript')
+  cy.get('#dropdowm-menu-3').should('have.value','javascript')
+  })
 
 
 
